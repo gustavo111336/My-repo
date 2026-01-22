337f9ee39808691bf8c4704079c42c25ac0c2a17
gustavo111336 <silvacostagustavo000@gmail.com>
2026-01-22 02:31:39 -0300

    Add a folder and put these files there.

---
+++ test.py
@@ -0,0 +1,10 @@
+# 1. Criamos um "pote" chamado helloworld e guardamos o texto "print" nele
+helloworld = "print"
+
+# 2. Criamos outro "pote" chamado print_personalizado e guardamos "hello world"
+# (Não usamos só 'print' para não estragar a função real do Python)
+texto_exibir = "hello world"
+
+# 3. Agora mandamos o Python mostrar o que está guardado neles
+print(helloworld)   # Vai aparecer: print
+print(texto_exibir) # Vai aparecer: hello world
